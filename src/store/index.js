4cb0f9ae80ce0bcb9e773daa73e0fd858fc0bb70
minami0601 <minami.love.bb@icloud.com>
2021-10-06 21:08:19 +0900

    カレンダー作成

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { event } from "@/store/event.js";
+
 Vue.use(Vuex);
 
 
 export default new Vuex.Store({
-	modules: {},
+	modules: {
+		event
+	},
 	// stateは参照専用、直接変更はNG
 	state: {
 		isOpenSideBar: true
